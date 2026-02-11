f75714e1e1589f95b6d17231d019db17049cd9f6
Hadi Diab <hadiibrahimdiab@gmail.com>
2026-02-11 13:33:14 +0200

    Update registration.routes.ts

--- src/routes/registration.routes.ts
+++ src/routes/registration.routes.ts
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { registerUserForEvent, getRegistrationsByUser, cancelRegistration  } from "../controllers/registration.controller";
+import { registerUserForEvent, getRegistrationsByUser, getRegistrationById, cancelRegistration  } from "../controllers/registration.controller";
 
 const router = Router();
 
 router.post("/", registerUserForEvent);
 router.get("/user/:id", getRegistrationsByUser);
+router.get("/:id", getRegistrationById);
 router.delete("/:id", cancelRegistration);
 
 export default router;
