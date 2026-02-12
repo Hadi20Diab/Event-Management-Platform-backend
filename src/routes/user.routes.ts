2c11b762e0468f6ad15f06cfd75dce31f41b1ca8
Hadi Diab <hadiibrahimdiab@gmail.com>
2026-02-12 11:51:30 +0200

    feat: implement authentication and authorization middleware for user routes

--- src/routes/user.routes.ts
+++ src/routes/user.routes.ts
@@ -6,13 +6,31 @@ import {
     updateUser,
     deleteUser
 } from "../controllers/user.controller";
+import { authenticate, authorizeAdmin, authorizeSelfOrAdmin } from "../middlewares/userAuth.middleware";
+import { signupUser } from "../controllers/userAuth.controller";
 
 const router = Router();
 
-router.post("/", createUser);
-router.get("/", getUsers);
-router.get("/:id", getUserById);
-router.put("/:id", updateUser);
-router.delete("/:id", deleteUser);
+// Create user: if request has Authorization header -> must be admin; otherwise public signup
+router.post("/", (req, res, next) => {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        // public signup
+        return signupUser(req, res, next);
+    }
+
+    // authenticated path -> require admin
+    authenticate(req, res, (err?: any) => {
+        if (err) return next(err);
+        authorizeAdmin(req, res, (err2?: any) => {
+            if (err2) return next(err2);
+            return createUser(req, res, next);
+        });
+    });
+});
+router.get("/", authenticate, authorizeAdmin, getUsers);
+router.get("/:id", authenticate, authorizeSelfOrAdmin, getUserById);
+router.put("/:id", authenticate, authorizeSelfOrAdmin, updateUser);
+router.delete("/:id", authenticate, authorizeSelfOrAdmin, deleteUser);
 
 export default router;
